4d2a2980ca72575a20677dfcbbe108a70a485613
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-01-16 21:10:31 +0530

    ...

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import style from "./navbar.module.css";
+// import style from "./navbar.module.css";
 
 interface Props {}
 
 export const Navbar = (props: Props) => {
   return (
-    <div className={style.nav}>
-      <Link to="/" className={style.navlink}>
-        Home
-      </Link>
-      <Link to="/tasks" className={style.navlink}>
-        Tasks
-      </Link>
-      <Link to="/tasks/add" className={style.navlink}>
-        Tasks
-      </Link>
-      <Link to="/login" className={style.navlink}>
-        Login
-      </Link>
-      <Link to="/register" className={style.navlink}>
-        Register
-      </Link>
-      <Link to="/summary" className={style.navlink}>
-        Summary
-      </Link>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/tasks">Tasks</Link>
+      <Link to="/tasks/add">Tasks</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/summary">Summary</Link>
     </div>
   );
 };
