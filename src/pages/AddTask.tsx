4dfe272a770dc9fef8d956088854396f20631cc8
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-01-16 22:55:21 +0530

    some mor echanges in tasks file

--- src/pages/AddTask.tsx
+++ src/pages/AddTask.tsx
@@ -6,10 +6,16 @@ interface Props {}
 export const AddTask = (props: Props) => {
   return (
     <Box>
-      <SimpleGrid>
-        <Box>1</Box>
-        <Box>2</Box>
-        <Box>3</Box>
+      <SimpleGrid spacing={"1rem"} columns={2}>
+        <Box bg="tomato" minW="10rem">
+          1
+        </Box>
+        <Box bg="tomato" minW="10rem">
+          2
+        </Box>
+        <Box bg="tomato" minW="10rem">
+          3
+        </Box>
       </SimpleGrid>
     </Box>
   );
